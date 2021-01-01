089cd44c42aed5f46fba1a3451dddbb36032ddf9
cg-Kdaf <kdblender@gmail.com>
2021-01-01 14:28:38 +0100

    Improving date showing and fonts

--- src/main.py
+++ src/main.py
@@ -1,64 +1,56 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding:utf-8 -*-
-import sys
-import os
-
-import logging
 import epd7in5_V2
-import time
-from datetime import datetime
 from PIL import Image,ImageDraw,ImageFont
-import traceback
+from datetime import datetime
 
 from weather import get_weather_data
 
-logging.basicConfig(level=logging.DEBUG)
+# Init display
+epd = epd7in5_V2.EPD()
+epd.init()
+Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+draw = ImageDraw.Draw(Himage)
 
-try:
-    epd = epd7in5_V2.EPD()
-    
-    print("...\nCleaning display ( ",epd.width,"by",epd.height,"px )")
-    epd.init()
-    epd.Clear()
+# Datas
+font_large = ImageFont.truetype('data/Font.ttc', 128)
+font_medium = ImageFont.truetype('data/Teko/Teko-Light.ttf', 32)
+font_weather = ImageFont.truetype('data/weather_font.ttf', 40)
+font_time_l = ImageFont.truetype('data/Orbitron/static/Orbitron-Regular.ttf', 64)
+font_time_s = ImageFont.truetype('data/Orbitron/static/Orbitron-SemiBold.ttf', 24)
 
-    font_large = ImageFont.truetype('data/Font.ttc', 128)
-    font_medium = ImageFont.truetype('data/Font.ttc', 32)
-    font_weather = ImageFont.truetype('data/weather_font.ttf', 40)
+print("...\nComputing image")
+# Draw time
+layout_w = (0,324)
+layout_h = (0,60)
+time_w = (layout_w[0],layout_w[0]+190)
 
-    # Drawing on the Horizontal image
-    print("...\nDisplaying time")
-    Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-    draw = ImageDraw.Draw(Himage)
-    draw.rectangle((0, 128, 324, 132), fill = 0)
-    draw.rectangle((324, 0, 328, 480), fill = 0)
+draw.rectangle((layout_w[0], layout_h[1], layout_w[1], layout_h[1]+4), fill = 0)
+draw.rectangle((layout_w[1], layout_h[0], layout_w[1]+4, epd.height), fill = 0)
 
-    date=datetime.now()
-    time_text = f"{date.hour:02d}:{date.minute:02d}"
-    draw.text((0, 0), time_text, font = font_large, fill = 0)
+now=datetime.now()
+time_text = now.strftime("%k:%M") # Time as 10:03 or 3:50
+text_w, text_h = draw.textsize(time_text, font=font_time_l)
+draw.text((time_w[0]+(time_w[1]-text_w)/2, layout_h[0]-15), time_text, font = font_time_l, fill = 0)
+date_text = now.strftime("%a %d\n%b %Y") # Date as Fri 01\nJan 2021
+draw.text((time_w[1], layout_h[0]), date_text, font = font_time_s, fill = 0)
 
-    draw.text((340,0), "Time", font = font_medium , fill = 0)
-    draw.text((500,0), "Temp", font = font_medium , fill = 0)
-    draw.text((620,0), "Trending", font = font_medium , fill = 0)
-    draw.rectangle((324, 31, 800, 33), fill = 0)
-    for index, data in enumerate(get_weather_data()):
-        draw.text((340,(index+1)*32), str(data[0]), font = font_medium, fill = 0) # Time
-        draw.text((500,(index+1)*32), f"{data[2]:4.1f}°C", font = font_medium , fill = 0) # Temp
-        draw.text((600,(index+1)*32), chr(data[3]), font = font_weather, fill = 0) # Icon
-        draw.text((620,(index+1)*32), str(data[1]), font = font_medium, fill = 0) # Trending
-        #if data[4] == "n":
-            # draw.rectangle
-            # TODO draw rectangle where the 
-        
-    epd.display(epd.getbuffer(Himage))
+# Draw weather
+draw.text((340,0), "Time", font = font_medium , fill = 0)
+draw.text((500,0), "Temp", font = font_medium , fill = 0)
+draw.text((620,0), "Trending", font = font_medium , fill = 0)
+draw.rectangle((324, 31, 800, 33), fill = 0)
+for index, data in enumerate(get_weather_data()):
+    draw.text((340,(index+1)*32), str(data[0]), font = font_medium, fill = 0) # Time
+    draw.text((500,(index+1)*32), f"{data[2]:4.1f}°C", font = font_medium , fill = 0) # Temp
+    draw.text((600,(index+1)*32), chr(data[3]), font = font_weather, fill = 0) # Icon
+    draw.text((620,(index+1)*32), str(data[1]), font = font_medium, fill = 0) # Trending
+    #if data[4] == "n":
+        # draw.rectangle
+        # TODO draw rectangle where the 
 
-    print("...\nSleeping")
-    epd.sleep()
-    epd.Dev_exit()
-
-except IOError as e:
-    logging.info(e)
-    
-except KeyboardInterrupt:
-    logging.info("ctrl + c:")
-    epd7in5_V2.epdconfig.module_exit()
-    exit()
+print("...\nDrawing image")
+epd.display(epd.getbuffer(Himage))
+print("...\nSleeping")
+epd.sleep()
+epd.Dev_exit()
