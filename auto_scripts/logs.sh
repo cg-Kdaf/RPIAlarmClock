e33c75e9ee6343abbe1139a8042e5b8bb96e0d1c
cg-Kdaf <kdblender@gmail.com>
2021-02-10 16:07:06 +0100

    Modifing log program

--- auto_scripts/logs.sh
+++ auto_scripts/logs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 
-journalctl -b -u AlarmClock.service --lines=30 --output=cat
+journalctl -b -u AlarmClock.service --lines=30 -q | cut --complement -d' ' -f4-5
 echo "\n\nPWR Programm\n\n"
-journalctl -b -u Utilities_PWR.service --lines=30 --output=cat
+journalctl -b -u Utilities_PWR.service --lines=30 -q | cut --complement -d' ' -f4-5
 
