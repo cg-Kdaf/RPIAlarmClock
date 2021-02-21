e507301d880a2dabb2e469d002fef1ace1c922af
cg-Kdaf <kdblender@gmail.com>
2021-02-21 12:16:32 +0100

    Small corrections for the linter

--- src/weather.py
+++ src/weather.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 import json
-import urllib.request
-import datetime
 
-city1_id="2996881"
-APPID="7ba1b7bb6563f08710a3a8138767bf32"
-unit="metric"
-openweathermap="http://api.openweathermap.org/data/2.5/forecast"
-month=("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
+city1_id = "2996881"
+APPID = "7ba1b7bb6563f08710a3a8138767bf32"
+unit = "metric"
+openweathermap = "http://api.openweathermap.org/data/2.5/forecast"
+month = ("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
 
-id_ranges=((200,233),(300,322),(500,532),(600,623),(701,782),(801,805))
+id_ranges = ((200, 233), (300, 322), (500, 532), (600, 623), (701, 782), (801, 805))
 # Correspond to thunderstorm, drizzle, rain, snow, atmosphere, clouds
 icon_correspondance = {
     "01": 97,
@@ -23,15 +21,15 @@ icon_correspondance = {
     "50": 98
 }
 
-def weather_intensity(weather_index):
-    for id_range in id_ranges:
-        if index in range(id_range[0],id_range[1]):
-            return (index-id_range[0])/(id_range[1]-id_range[0])*100
+# def weather_intensity(weather_index):
+#     for id_range in id_ranges:
+#         if index in range(id_range[0],id_range[1]):
+#             return (index-id_range[0])/(id_range[1]-id_range[0])*100
 
 # Icon correspondance between openweathermap (https://openweathermap.org/weather-conditions)
 # and the ascii caracter of the font (https://www.dafont.com/weather.font)
 
 
 def get_weather_data():
-    infos=json.loads(open("/home/pi/AlarmClockProject/AlarmClock/cache/weathers/weather0.json", "r").read())
+    infos = json.loads(open("/home/pi/AlarmClockProject/AlarmClock/cache/weathers/weather0.json", "r").read())
     return infos["list"]
