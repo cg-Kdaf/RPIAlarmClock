f901f3c6074908ef68978f94d2b3563d99e1fa96
cg-Kdaf <kdblender@gmail.com>
2021-02-24 21:13:23 +0100

    Cleaning some stuff

--- src/sound.py
+++ src/sound.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 from time import sleep as time_sleep
 from music_commands import Music_lib
 from os import listdir
 
 
-def ring():
-    speaker_pin = 13  # GPIO Pin
-    dutycycle = 10
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(speaker_pin, GPIO.OUT)
-    __buzzer = GPIO.PWM(speaker_pin, 1000)  # Set frequency to 1000hz (initial value, change to something else afterward)
-    __buzzer.start(dutycycle)
-    for i in range(10):
-        __buzzer.ChangeFrequency(440)
-        time_sleep(.3)
-        __buzzer.ChangeFrequency(600)
-        time_sleep(.3)
-    __buzzer.stop()
-    GPIO.cleanup()
+# def ring():
+#     speaker_pin = 13  # GPIO Pin
+#     dutycycle = 10
+#     GPIO.setmode(GPIO.BCM)
+#     GPIO.setup(speaker_pin, GPIO.OUT)
+#     __buzzer = GPIO.PWM(speaker_pin, 1000)  # Set frequency to 1000hz (initial value, change to something else afterward)
+#     __buzzer.start(dutycycle)
+#     for i in range(10):
+#         __buzzer.ChangeFrequency(440)
+#         time_sleep(.3)
+#         __buzzer.ChangeFrequency(600)
+#         time_sleep(.3)
+#     __buzzer.stop()
+#     GPIO.cleanup()
 
 
 def mocp_play():
