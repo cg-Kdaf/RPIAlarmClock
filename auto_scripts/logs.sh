52a150ce9d8b6bf74176e9f4f5865e73ffdd5aa4
cg-Kdaf <kdblender@gmail.com>
2021-02-28 10:10:31 +0100

    Adding cron logging

--- auto_scripts/logs.sh
+++ auto_scripts/logs.sh
@@ -1,6 +1,14 @@
 #!/bin/sh 
 
-journalctl -b -u AlarmClock.service --lines=30 -q | cut --complement -d' ' -f4-5
-echo "\n\nPWR Programm\n\n"
-journalctl -b -u Utilities_PWR.service --lines=30 -q | cut --complement -d' ' -f4-5
+lines=30
 
+if [ $# != "0" ]
+then
+    lines=${1}
+fi
+
+journalctl -b -u AlarmClock.service --lines=${lines} -q | cut --complement -d' ' -f4-5
+echo "\n\nPWR Programm\n\n"
+journalctl -b -u Utilities_PWR.service --lines=${lines} -q | cut --complement -d' ' -f4-5
+echo "\n\nCrontab\n\n"
+grep --ignore-case CRON /var/log/syslog | tail -${lines} | cut --complement -d' ' -f4-5
