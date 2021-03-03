1628b55f3cc14795ea21213f5f4f637c1c321c7e
cg-Kdaf <kdblender@gmail.com>
2021-03-03 17:47:10 +0100

    Adding deezer to the auto refresh tools

--- src/update_data.sh
+++ src/update_data.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Get the calendars
+echo 'Get calendar'
 index=0
 
 while IFS= read -r line; do
@@ -9,6 +10,7 @@ while IFS= read -r line; do
 done < /home/pi/AlarmClockProject/AlarmClock/data/calendars.txt
 
 # Get the weather
+echo 'Get weather data'
 
 index=0
 
@@ -18,9 +20,16 @@ while IFS= read -r line; do
 done < /home/pi/AlarmClockProject/AlarmClock/data/weathers.txt
 
 # Get the tasks
+echo 'Get tasks'
 
 python3 /home/pi/AlarmClockProject/AlarmClock/src/google_tasks.py
 
 # Get the ecoledirect data
+echo 'Get ED datas'
 
 python3 /home/pi/AlarmClockProject/AlarmClock/src/ED_utilities.py
+
+# Get the deezer datas
+echo 'Get deezer musics'
+
+python3 /home/pi/AlarmClockProject/AlarmClock/src/deezer_musics.py
