450824fe8d095baa4fac7fd561fbd8b79d46da8e
Kdaf <kdblender@gmail.com>
2021-11-11 08:08:08 +0100

    Filtering of the musics

--- src/sound.py
+++ src/sound.py
@@ -2,30 +2,22 @@
 # import RPi.GPIO as GPIO
 from music_commands import Music_lib
 from os import listdir
-
-
-# def ring():
-#     speaker_pin = 13  # GPIO Pin
-#     dutycycle = 10
-#     GPIO.setmode(GPIO.BCM)
-#     GPIO.setup(speaker_pin, GPIO.OUT)
-#     __buzzer = GPIO.PWM(speaker_pin, 1000)  # Set frequency to 1000hz (initial value, change to something else afterward)
-#     __buzzer.start(dutycycle)
-#     for i in range(10):
-#         __buzzer.ChangeFrequency(440)
-#         time_sleep(.3)
-#         __buzzer.ChangeFrequency(600)
-#         time_sleep(.3)
-#     __buzzer.stop()
-#     GPIO.cleanup()
+from system_commands import send_signal
 
 
 def mocp_play():
+    send_signal("main.py", "SIGUSR1")
     mocp = Music_lib()
     mocp.queue_clear_mocp()
     playlists = listdir('/home/pi/Music/')
     first_playlist = [p_name for p_name in playlists if "First" in p_name]
-    first_music = listdir(f"/home/pi/Music/{first_playlist[0]}/")[0]
+    first_music = None
+    for file in listdir(f"/home/pi/Music/{first_playlist[0]}/"):
+        if ".lrc" in file:
+            continue
+        first_music = file
+        break
+    print(first_music)
     first_music_path = f"/home/pi/Music/{first_playlist[0]}/{first_music}"
     mocp.queue_add_mocp(f'"{first_music_path}"')
     playlists.pop(playlists.index(first_playlist[0]))
@@ -36,4 +28,3 @@ def mocp_play():
 
 if __name__ == "__main__":
     mocp_play()
-    # ring()
